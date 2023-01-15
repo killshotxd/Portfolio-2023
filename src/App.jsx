6c1c2a0b003d8ab730bf84115acbb52dd01fbe1b
killshotxd <hassanansari211@gmail.com>
2023-01-15 17:46:21 +0530

    updated style of cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 const App = () => {
+  const [cursorXY, setCursorXY] = useState({ x: -100, y: -100 });
+  useEffect(() => {
+    const moveCursor = (e) => {
+      const x = e.clientX - 16;
+      const y = e.clientY - 16;
+      setCursorXY({ x, y });
+    };
+    window.addEventListener("mousemove", moveCursor);
+    return () => {
+      window.removeEventListener("mousemove", moveCursor);
+    };
+  }, []);
   return (
     <BrowserRouter>
+      <div
+        className="cursor"
+        style={{
+          transform: `translate3d(${cursorXY.x}px, ${cursorXY.y}px, 0)`,
+        }}
+      />
+
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useRef } from "react";
 import styles from "./Home.module.css";
 import { BsGithub } from "react-icons/bs";
 import Typewriter from "typewriter-effect";
 const Home = () => {
+  const myRef = useRef(null);
+  const executeScroll = () => myRef.current.scrollIntoView();
   return (
     <>
       <div className={styles.parallax}>
@@ -11,49 +13,85 @@ const Home = () => {
         <div id={styles.stars3}></div>
 
         {/* START HOME */}
-
-        <div className="container">
-          <div className="row">
-            <div className="col ">
-              <div
-                style={{ cursor: "pointer", width: "fit-content !important" }}
-                className="d-flex align-items-center justify-content-center gap-2"
-              >
-                <BsGithub size={40} />
-                <p className="m-0 p-0 fs-2">
-                  {" "}
-                  <Typewriter
-                    options={{
-                      strings: ["Mohd Hassan", "Web Developer"],
-                      autoStart: true,
-                      loop: true,
-                    }}
-                  />
-                </p>
+        <section id="1" style={{ height: "100vh" }}>
+          <div className="container">
+            <div className="row">
+              <div className="col ">
+                <div
+                  style={{ cursor: "pointer", width: "fit-content !important" }}
+                  className="d-flex align-items-center justify-content-center gap-2"
+                >
+                  <BsGithub size={40} />
+                  <p className="m-0 p-0 fs-2 fw-bolder">
+                    {" "}
+                    <Typewriter
+                      options={{
+                        strings: ["Mohd Hassan", "Web Developer"],
+                        autoStart: true,
+                        loop: true,
+                      }}
+                    />
+                  </p>
+                </div>
               </div>
-            </div>
-            <div className="col">
-              <div className="d-flex align-items-center justify-content-center gap-5">
-                <p className={`m-0 p-0 fs-3 ${styles.hvr}`}>Connect?</p>
-                <p className={`m-0 p-0 fs-3 ${styles.hvr}`}>Projects</p>
+              <div className="col">
+                <div className="d-flex align-items-center justify-content-center gap-5">
+                  <p className={`m-0 p-0 fs-3 fw-bolder ${styles.hvr}`}>
+                    Connect?
+                  </p>
+                  <p className={`m-0 p-0 fs-3 fw-bolder ${styles.hvr}`}>
+                    Projects
+                  </p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
 
-        {/* HEADER END */}
+          {/* HEADER END */}
 
-        {/* HEADINGS */}
+          {/* HEADINGS */}
 
-        <div className="container">
-          <div className={styles.dot}></div>
-          <div className={styles.dot}></div>
-          <div className={styles.dot}></div>
-          <div className={styles.dot}></div>
-          <div className={styles.dot}></div>
-        </div>
+          <div
+            className="container d-flex flex-column justify-content-center align-items-center"
+            style={{ marginTop: "8rem" }}
+          >
+            <h2 className="fw-bolder">
+              Hello, Myself{" "}
+              <span style={{ color: "#005b87" }}>MOHD HASSAN</span> 😁
+            </h2>
+            <h3 className="fw-bolder mt-3">
+              Welcome to my Portfolio ({new Date().getFullYear()})
+            </h3>
+            <h3 className="fw-bolder mt-3">
+              {" "}
+              <Typewriter
+                options={{
+                  strings: [
+                    "I'm a Frontend Web Developer",
+                    "I'm Familiar with these Tech Stacks :",
+                    "HTML, CSS, JavaScript, NodeJs, ReactJs, NextJs, FireBase, TailwindCss and BootStrap",
+                    "Explore More 👇🏻",
+                  ],
+                  autoStart: true,
+                  loop: true,
+                }}
+              />
+            </h3>
 
-        {/* HEADINGS END */}
+            <button
+              onClick={executeScroll}
+              className={` mt-3 ${styles.button55}`}
+            >
+              Know More
+            </button>
+          </div>
+
+          {/* HEADINGS END */}
+        </section>
+
+        <section style={{ height: "100vh" }} ref={myRef} id="2">
+          Hello
+        </section>
       </div>
     </>
   );
