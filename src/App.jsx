63078ae85ca4e2ac39f29095ae30ae394dc87933
killshotxd <hassanansari211@gmail.com>
2023-03-21 12:24:14 +0530

    lazy load components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, lazy, Suspense } from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./components/Home/Home";
+// import Home from "./components/Home/Home";
 import Header from "./components/Header/Header";
-import Section2 from "./components/Sections/Section2";
-import Projects from "./components/Projects/Projects";
+// import Section2 from "./components/Sections/Section2";
+// import Projects from "./components/Projects/Projects";
+import Lottie from "react-lottie";
+import load from "./assets/98431-loading-animation.json";
 import { FaDownload } from "react-icons/fa";
 import {
   BsFillArrowLeftCircleFill,
   BsFillArrowRightCircleFill,
 } from "react-icons/bs";
+
+const Home = lazy(() => import("./components/Home/Home"));
+const Section2 = lazy(() => import("./components/Sections/Section2"));
+const Projects = lazy(() => import("./components/Projects/Projects"));
+
 const App = () => {
   const [cursorXY, setCursorXY] = useState({ x: -100, y: -100 });
   const [visible, setVisible] = useState(true);
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: load,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
   useEffect(() => {
     const moveCursor = (e) => {
       const x = e.clientX - 16;
@@ -66,11 +83,25 @@ const App = () => {
           </div>
         )}
       </>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/exp" element={<Section2 />} />
-        <Route path="/projects" element={<Projects />} />
-      </Routes>
+      <Suspense
+        fallback={
+          <div className="d-flex m-auto justify-content-center align-items-center h-100">
+            {" "}
+            <Lottie
+              options={defaultOptions}
+              height={100}
+              width={100}
+              style={{ margin: 0 }}
+            />
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/exp" element={<Section2 />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
